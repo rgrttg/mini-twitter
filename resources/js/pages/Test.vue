fae5590d161558c8224b705d1ac4db6fe5bc97dd
rgrttg <ottiger@outlook.de>
2024-03-20 10:59:49 +0100

    Link zur Homepage

---
+++ resources/js/pages/Test.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>Testseite</h1>
+    <!-- <router-link to="/">Link zur Home Seite</router-link> -->
+    <router-link to="/all">Link zur Mini-Twitter Seite</router-link>
+</template>
