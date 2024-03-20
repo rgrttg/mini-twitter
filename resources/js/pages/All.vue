266eda8f24376824609f28a758558a7adf61a40b
rgrttg <ottiger@outlook.de>
2024-03-20 11:01:04 +0100

    Alle Tweets anzeigen

---
+++ resources/js/pages/All.vue
@@ -0,0 +1,34 @@
+
+<script setup>
+    import axios from "axios";
+    import { ref } from "vue";
+
+    const response = ref();
+
+    const getValue = async () => {
+        try {
+            let apiResponse = await axios.get("/api/all");
+            // console.log(apiResponse);
+            response.value = apiResponse.data;
+            console.log(response.value);
+        } catch (error) {
+            // Do something with the error
+            console.log(error);
+        }
+    };
+
+    getValue();
+</script>
+
+<template>
+  <h1>Alle Tweets</h1>
+    <ul>
+      <li v-for="tweet in response ">
+        {{ tweet.title }}<br>
+        {{ tweet.text }}
+      </li>
+    </ul>
+
+  <!-- <router-link to="/">Link zur Home Seite</router-link> -->
+  <router-link to="/new">Neuen Tweet erstellen</router-link>
+</template>
